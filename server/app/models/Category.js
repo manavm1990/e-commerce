1b37b000801d016a1b6d74ec68769a2b278062e9
Manav Misra <manavm@visionwebsoft.com>
2021-10-04 13:34:45 -0500

    :coffin: router.js

--- server/app/models/Category.js
+++ server/app/models/Category.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const categorySchema =  new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-})
-
-// Convert schema to model
-export default mongoose.model('Category', categorySchema);
+export default mongoose.model(
+  "Category",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  })
+);
