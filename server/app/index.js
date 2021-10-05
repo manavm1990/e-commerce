e6a94427582b3fe4796856e1fcfc573d89be31bc
Manav Misra <manavm@visionwebsoft.com>
2021-10-05 15:04:20 -0500

    Serve up static images

--- server/app/index.js
+++ server/app/index.js
@@ -2,10 +2,15 @@ import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import http from "http";
+import { URL } from "url";
 import dbClient from "./db/client.js";
 import { resolvers, typeDefs } from "./graphql/index.js";
 
 const app = express();
+
+// Serve static images for products
+app.use(express.static(new URL("public", import.meta.url).pathname));
+
 const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
