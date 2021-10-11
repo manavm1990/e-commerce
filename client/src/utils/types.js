82629ab4dd273b8c457f8e62119aa81544be262e
Manav Misra <manavm@visionwebsoft.com>
2021-10-11 08:15:11 -0500

    :mute: Avoid using `exact` for object prop types

--- client/src/utils/types.js
+++ client/src/utils/types.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
-export const Product = PropTypes.exact({
-  _id: PropTypes.number.isRequired,
+export const Product = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   description: PropTypes.string,
   image: PropTypes.string,
   quantity: PropTypes.number,
-  category: PropTypes.exact({ name: PropTypes.string.isRequired }),
+  category: PropTypes.shape({ name: PropTypes.string.isRequired }),
 });
