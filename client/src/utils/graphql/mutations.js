3f000bacda42628e236a1de9a4e9d0f859b7ca0b
Manav Misra <manavm@visionwebsoft.com>
2021-10-11 15:34:28 -0500

    :seedling: ADD_USER

---
+++ client/src/utils/graphql/mutations.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation AddUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
