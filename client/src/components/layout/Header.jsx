61fb563c6d569c90839198051fe885687b0b39d7
Manav Misra <manavm@visionwebsoft.com>
2021-10-11 13:04:59 -0500

    Conditionally render Actions based on pathName
    Replaces buttons with Links.

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,20 +1,32 @@
 import config from "@app/utils/config";
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
   return (
     <header className="px-8 flex flex-col">
       <h1 className="font-extrabold text-2xl text-center my-4">
         {config.siteName}
       </h1>
-      <div className="flex justify-between mb-2">
-        <button className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full w-max">
+      <div className="flex gap-8 justify-center mb-2">
+        {location.pathname !== "/login" && (
+          <Link
+            to="/login"
+            className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full w-max"
+          >
           Login
-        </button>
+          </Link>
+        )}
 
-        <button className="bg-yellow-500 hover:bg-yellow-700 text-white py-2 px-4 rounded-full w-max">
+        {location.pathname !== "/create-account" && (
+          <Link
+            to="/create-account"
+            className="bg-yellow-500 hover:bg-yellow-700 text-white py-2 px-4 rounded-full w-max"
+          >
           Create Account
-        </button>
+          </Link>
+        )}
       </div>
     </header>
   );
