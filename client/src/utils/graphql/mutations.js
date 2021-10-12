0db7d79bad632314dbbb5ca63388862da3603c11
Manav Misra <manavm@visionwebsoft.com>
2021-10-12 07:47:16 -0500

    Add LOGIN mutation in utils

--- client/src/utils/graphql/mutations.js
+++ client/src/utils/graphql/mutations.js
@@ -23,3 +23,15 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        firstName
+        _id
+      }
+    }
+  }
+`;
